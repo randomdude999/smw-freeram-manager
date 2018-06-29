867149b5ebf95fac1d8a76ba22d3f370c14bf9a3
randomdude999 <just.so.you.can.email.me@gmail.com>
2018-06-30 01:12:39 +0300

    make test app more flexible

--- test.c
+++ test.c
@@ -1,18 +1,29 @@
 #include "interface/freeram.h"
 #include <stdio.h>
 
-int main() {
+int main(int argc, char** argv) {
 	if(!freeram_loadlib()) {
-		fprintf(stderr, "Couldn't load library\n");
+		printf("Error: Couldn't load library\n");
+		return 1;
+	}
+	if(argc != 5) {
+		printf("Usage: %s <romname> <id> <size> <flags>\n", argv[0]);
 		return 1;
 	}
 	char* error;
-	freeram_handle* handle = freeram_open("test.smc", &error);
+	freeram_handle* handle = freeram_open(argv[1], &error);
 	if(!handle) {
-		fprintf(stderr, "Couldn't load ramfile: %s\n", error);
+		printf("Couldn't load ramfile: %s\n", error);
 		return 1;
 	}
-	fprintf(stderr, "Claimed freeram: %08X\n", freeram_get_ram(handle, 2, "test", "addr clear_ow"));
+	int size;
+	sscanf(argv[3], "%i", &size);
+	int freeram = freeram_get_ram(handle, size, argv[2], argv[4]);
+	if(freeram < 0) {
+		printf("Error: get_ram returned %d\n", freeram);
+	} else {
+		printf("Claimed freeram: $%06X\n", freeram);
+	}
 
 	freeram_close(handle);
 	freeram_unloadlib();
